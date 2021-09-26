e03e15adc1db7c02a7005287585696e8c34d3b97
imgVOID <imgvoid@gmail.com>
2021-09-26 03:11:50 +0300

    metadata

--- main.py
+++ main.py
@@ -3,11 +3,23 @@ from fastapi import FastAPI
 from utilities.docker_scripts import DockerUtils
 from routers.tasks import router_tasks
 from routers.check import router_check
-
+from utilities.app_metadata import tags_metadata, app_metadata_description
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app = FastAPI()
-app.include_router(router_tasks)
-app.include_router(router_check)
 DockerUtils.fix_docker_bug()
 
+app = FastAPI(title='Autograde-API',
+              description=app_metadata_description,
+              version='0.0.1',
+              contact={
+                  "name": "Maria Hladka",
+                  "url": "https://github.com/imgVOID",
+                  "email": "imgvoid@gmail.com",
+              },
+              license_info={
+                  "name": "Apache 2.0",
+                  "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+              }, openapi_tags=tags_metadata)
+
+app.include_router(router_tasks)
+app.include_router(router_check)
---
+++ utilities/app_metadata.py
@@ -0,0 +1,20 @@
+tags_metadata = [
+    {
+        "name": "tasks",
+        "description": "Task management with description, solution code, input and output data.",
+    },
+    {
+        "name": "check",
+        "description": "Checking if the user's answer is correct: "
+                       "running the code safely in the Docker container and comparing the result."
+    },
+]
+
+app_metadata_description = """
+### 🚀 Fast and asynchronous automatic assignments grading API:\n
+1. *Based on the FastAPI and Python Docker SDK.*\n
+2. *Separated one-time Docker containers for the user input check.*\n
+3. *Totally RESTful (CRUD) API for use with a custom frontend.*\n
+* **Tasks**: CRUD assignments management.
+* **Check**: user input validation by the safely run in a disposable Docker container.
+"""
