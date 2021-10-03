0ddb66a3c8370aa249ab37241892a2b04f2c86cc
Maria Hl <imgvoid@gmail.com>
2021-10-03 21:34:02 +0300

    docstrings

--- utilities/file_scripts.py
+++ utilities/file_scripts.py
@@ -9,6 +9,13 @@ APP_ROOT = dirname(dirname(abspath(__file__)))
 
 
 def get_filepath(title, theme_id, task_id):
+    """
+    `get_filepath` function returns file path 
+    It takes three parameters: 
+    1. `title` has four variants: "task_info", "task_input", "task_output", "task_code".
+    2. `theme_id` means an id of the theme and the directory name.
+    3. `task_id means` an id of the task in a theme and a part of the file name.
+    """
     filesystem = {
         "task_info": join(APP_ROOT,
                           'materials', f'{theme_id}',
@@ -27,6 +34,13 @@ def get_filepath(title, theme_id, task_id):
 
 
 async def save_user_input(code, theme, task_id):
+    """
+    `save_user_input` function returns file name and random number.
+    It takes three parameters: 
+    1. `code` is the bytes object with the user input untrusted code.
+    2. `theme` means an id of the theme and the directory name.
+    3. `task_id` means an id of the task in a theme and a part of the file name.
+    """
     random_id = random.randint(0, 100000)
     filename = join(APP_ROOT,
                     f'materials/{theme}/inputs/',
