4c5d646df6c5afbfdb3da10dc5bdac0a4358ac61
imgVOID <imgvoid@gmail.com>
2021-10-12 19:15:39 +0300

    some schemas decorative changes

--- schemas/tasks.py
+++ schemas/tasks.py
@@ -1,6 +1,10 @@
+from json import loads
 from pydantic import BaseModel
 from typing import Optional
-import json
+
+
+class NotFoundMessage(BaseModel):
+    message: str = "Task not found"
 
 
 class Task(BaseModel):
@@ -10,49 +14,12 @@ class Task(BaseModel):
     input: list[str]
     output: list[str]
 
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate_to_json
-
-    @classmethod
-    def validate_to_json(cls, value):
-        if isinstance(value, str):
-            return cls(**json.loads(value))
-        return value
-
     class Config:
         extra = 'allow'
         schema_extra = {
             "example": {
                 "id": 0,
                 "theme_id": 0,
-                "description": ["Task's essence.",
-                                "Separated by a newline."],
-                "input": ["First input", "2"],
-                "output": ["First output", "2"],
-            }
-        }
-
-
-class TaskUpdate(BaseModel):
-    description: Optional[list[str]] = None
-    input: Optional[list[str]] = None
-    output: Optional[list[str]] = None
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate_to_json
-
-    @classmethod
-    def validate_to_json(cls, value):
-        if isinstance(value, str):
-            return cls(**json.loads(value))
-        return value
-
-    class Config:
-        extra = 'allow'
-        schema_extra = {
-            "example": {
                 "description": ["Task's essence.",
                                 "Separated by a newline."],
                 "input": ["First input", "2"],
@@ -74,7 +41,7 @@ class TaskCreate(BaseModel):
     @classmethod
     def validate_to_json(cls, value):
         if isinstance(value, str):
-            return cls(**json.loads(value))
+            return cls(**loads(value))
         return value
 
     class Config:
@@ -90,5 +57,28 @@ class TaskCreate(BaseModel):
         }
 
 
-class NotFoundMessage(BaseModel):
-    message: str = "Task not found"
+class TaskUpdate(BaseModel):
+    description: Optional[list[str]] = None
+    input: Optional[list[str]] = None
+    output: Optional[list[str]] = None
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate_to_json
+
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**loads(value))
+        return value
+
+    class Config:
+        extra = 'allow'
+        schema_extra = {
+            "example": {
+                "description": ["If you want to change only the code",
+                                "please send an empty dict."],
+                "input": ["First input", "2"],
+                "output": ["First output", "2"],
+            }
+        }
