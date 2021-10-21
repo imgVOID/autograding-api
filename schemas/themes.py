8ac0a027641e0489e7b32abf15d8aba4a042927f
imgVOID <imgvoid@gmail.com>
2021-10-21 04:39:28 +0300

    docstrings

--- schemas/themes.py
+++ schemas/themes.py
@@ -4,6 +4,10 @@ from schemas.tasks import Task
 
 
 class Theme(BaseModel):
+    """
+    `Theme` is a pydantic model defining the schema
+    for getting a full theme info (including task list) via GET requests.
+    """
     theme_id: int
     theme_name: str
     tasks_count: int
