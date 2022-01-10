28c6127e9ce71ceed79626a42d2849ee5f2305bd
imgVOID <imgvoid@gmail.com>
2022-01-10 17:01:08 +0200

    tests fix for the travis ci

--- tests/travis/test_auth.py
+++ tests/travis/test_auth.py
@@ -4,31 +4,6 @@ from os.path import abspath, dirname, join
 from main import app
 
 
-class TestTopicsCRUDAsync:
-    def test_bearer_token(self):
-        client = TestClient(app)
-        # Please create new user with the "credentials.json" info
-        with open(join(dirname(abspath(__file__)), 'data', 'credentials.json'),
-                  mode='r', encoding='utf-8') as f:
-            example_user = loads(f.read())
-            data = {
-                'username': example_user['email'],
-                'password': example_user['password'],
-                'grant_type': '', 'scope': '', 'client_id': '', 'client_secret': ''
-            }
-            headers = {
-                'accept': 'application/json',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-            response = client.post(f"/auth/token", data=data, headers=headers)
-
-        try:
-            assert response.status_code == 200
-            assert isinstance(response.json()['access_token'], str)
-        except (KeyError, AttributeError) as e:
-            raise ValueError("There is no user who have already registered with this email address.") from e
-
-
 class TestTopicsErrorsAsync:
     def test_create_user_fail(self):
         client = TestClient(app)
@@ -57,3 +32,27 @@ class TestTopicsErrorsAsync:
         assert response.status_code == 400
         assert response.json()['detail'] == 'There is no user who have already registered with this email address.'
 
+
+class TestTopicsCRUDAsync:
+    def test_bearer_token(self):
+        client = TestClient(app)
+        # Please create new user with the "credentials.json" info
+        with open(join(dirname(abspath(__file__)), 'data', 'credentials.json'),
+                  mode='r', encoding='utf-8') as f:
+            example_user = loads(f.read())
+            data = {
+                'username': example_user['email'],
+                'password': example_user['password'],
+                'grant_type': '', 'scope': '', 'client_id': '', 'client_secret': ''
+            }
+            headers = {
+                'accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            response = client.post(f"/auth/token", data=data, headers=headers)
+
+        try:
+            assert response.status_code == 200
+            assert isinstance(response.json()['access_token'], str)
+        except (KeyError, AttributeError) as e:
+            raise ValueError("There is no user who have already registered with this email address.") from e
